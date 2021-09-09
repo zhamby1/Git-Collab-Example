35a1bfeb59940480939f521a2fb095e58a48fcee
Zach Hamby <>
2021-09-09 17:50:19 +0000

    added new print line for galaxy -zh

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 #repo stands for repository.  Basically a folder that has been marked to be used with version control. You create a repo per new piece of software
 
+#committed the software (hard save) to the current branch
+
 print("Hello World")
+print("Hello Galaxy")
