cde9c187738e85bbdb25d700d33cf1108a8c432f
Zach Hamby <>
2021-09-09 17:51:37 +0000

    changed to say hello universe -zh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 #repo stands for repository.  Basically a folder that has been marked to be used with version control. You create a repo per new piece of software
 
-#committed the software (hard save) to the current branch
-
 print("Hello World")
-print("Hello Galaxy")
+print("Hello Universe")
